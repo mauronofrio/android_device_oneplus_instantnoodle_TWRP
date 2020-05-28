14e34b0c8d38a40be7a249050cfcae67b46a1ca7
mauronofrio <poronofrio@gmail.com>
2020-05-28 23:13:53 +0200

    Add unified script

---
+++ recovery/root/sbin/unified-script.sh
@@ -0,0 +1,48 @@
+#!/sbin/sh
+# This script is needed to automatically set device props.
+
+
+load_op8pro()
+{
+	echo resetting
+    resetprop "ro.product.model" "OnePlus 8 Pro"
+    resetprop "ro.product.name" "OnePlus8Pro"
+    resetprop "ro.build.product" "OnePlus8Pro"
+    resetprop "ro.product.device" "OnePlus8Pro"
+    resetprop "ro.vendor.product.device" "OnePlus8Pro"
+    resetprop "ro.display.series" "OnePlus 8 Pro"
+}
+
+load_op8()
+{
+    resetprop "ro.product.model" "OnePlus 8"
+    resetprop "ro.product.name" "OnePlus8"
+    resetprop "ro.build.product" "OnePlus8"
+    resetprop "ro.product.device" "OnePlus8"
+    resetprop "ro.vendor.product.device" "OnePlus8"
+    resetprop "ro.display.series" "OnePlus 8"
+}
+
+project=$(getprop ro.boot.project_name)
+echo $project
+
+case $project in
+    "19811")
+		echo loading
+        load_op8pro
+        ;;
+    "19855")
+        load_op8pro
+        ;;
+    "19867")
+        load_op8pro
+        ;;
+    "19821")
+        load_op8
+        ;;
+    *)
+        load_op8pro
+        ;;
+esac
+
+exit 0
