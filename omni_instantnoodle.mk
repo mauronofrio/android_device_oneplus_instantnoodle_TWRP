b9fec040a7d6459b2b79283514d862f980057cb5
mauronofrio <poronofrio@gmail.com>
2020-06-05 21:27:19 +0200

    Move to core_minimal

--- omni_instantnoodle.mk
+++ omni_instantnoodle.mk
@@ -17,7 +17,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := instantnoodle
 
-$(call inherit-product, build/target/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
